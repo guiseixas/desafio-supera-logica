9b80277e9d1c74c811d572c78d86d4781c4748e6
guilherme <guilherme.santos@dellead.com>
2023-07-12 20:04:12 -0300

    finished challange 01

--- desafio-01/src/Desafio01.java
+++ desafio-01/src/Desafio01.java
@@ -1,5 +1,37 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Desafio01 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+
+        ArrayList<Integer> inputs = new ArrayList<Integer>();
+        ArrayList<Integer> valoresPares = new ArrayList<Integer>();
+        ArrayList<Integer> valoresImpares = new ArrayList<Integer>();
+
+        for(int i = 0; i < N; i++){
+            int valor = scanner.nextInt();
+            inputs.add(valor);
+
+            if(inputs.get(i) % 2 == 0){
+                valoresPares.add(inputs.get(i));
+            }else{
+                valoresImpares.add(inputs.get(i));
+            }
+        }
+
+        Collections.sort(valoresPares);
+        Collections.sort(valoresImpares, Collections.reverseOrder());
+
+        for(int valorPar : valoresPares){
+            System.out.println(valorPar);
+        }
+
+        for(int valorImpar : valoresImpares){
+            System.out.println(valorImpar);
+        }
     }
 }
