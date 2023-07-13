db55919c421d763b615d401c8ba3bd0759ecfcc9
guilherme <guilherme.santos@dellead.com>
2023-07-13 10:27:30 -0300

    finished challenge 04

--- desafio-04/src/Desafio04.java
+++ desafio-04/src/Desafio04.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Desafio04 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        scanner.nextLine();
+
+        StringBuilder resultadoFinal = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            String linha = scanner.nextLine();
+            StringBuilder primeiraMetade = new StringBuilder(linha.substring(0, linha.length() / 2));
+            StringBuilder segundaMetade  = new StringBuilder(linha.substring(linha.length() / 2));
+            primeiraMetade.reverse();
+            segundaMetade .reverse();
+            resultadoFinal.append(primeiraMetade).append(segundaMetade );
+
+            //Evitar excesso de quebra de linha
+            if (i != N - 1) {
+                resultadoFinal.append("\n");
+            }
+        }
+
+        System.out.println(resultadoFinal.toString());
     }
 }
