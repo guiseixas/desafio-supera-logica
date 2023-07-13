538c240dc3c0e518a9174f7e03582922a5537edb
guilherme <guilherme.santos@dellead.com>
2023-07-13 09:30:44 -0300

    finished challenge 03

--- desafio-03/src/Desafio03.java
+++ desafio-03/src/Desafio03.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
+import static java.lang.Math.abs;
+
 public class Desafio03 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        int k = scanner.nextInt();
+
+        int[] array = new int[n];
+        for(int i = 0; i < n; i++){
+            array[i] = scanner.nextInt();
+        }
+
+        int contador = 0;
+        for(int i = 0; i < array.length; i++){
+            for(int j = i + 1; j < array.length; j++){
+                if(abs(array[i] - array[j]) == k){ //Retorno do valor absoluto na diferença array[i] - array[j] função abs
+                    contador++;
+                }
+            }
+        }
+
+        System.out.println(contador);
     }
 }
